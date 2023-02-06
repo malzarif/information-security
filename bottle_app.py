86bff7e699a327baf3de847b2b285c8f3f0eaedb
Mohamad AlZarif <123195209+malzarif@users.noreply.github.com>
2023-02-06 23:59:24 +0000

    Added cookies

--- bottle_app.py
+++ bottle_app.py
@@ -1,5 +1,5 @@
 import os
-from bottle import default_app, route, run, redirect
+from bottle import default_app, route, run, redirect, request, response
 
 @route('/')
 def get_index():
@@ -11,14 +11,19 @@ def get_public():
 
 @route('/secret')
 def get_secret():
-    return 'This secret message should only be shown to authorized people!'
+    if request.get_cookie("user","false")=="true":
+        return 'This secret message should only be shown to authorized people!'
+    else:
+        return 'Nothing'
 
 @route('/login')
 def get_login():
+    response.set_cookie("user","true")
     return("ok, it looks like you logged in")
 
 @route('/logout')
 def get_logout():
+    response.set_cookie("user","false")
     return("ok, it looks like you logged out")
 
 if "PYTHONANYWHERE_DOMAIN" in os.environ:
