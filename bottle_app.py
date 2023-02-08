24937e8377d8751fad96dbdb7c32a2030adb6178
Mohamad AlZarif <123195209+malzarif@users.noreply.github.com>
2023-02-07 20:29:15 -0500

    updated password to include hashing

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,15 @@
+import hashlib
 import json
 import os
 from bottle import default_app, route, run, redirect, request, response
+
+def hash_password(password):
+    password_bytes = password.encode('utf-8')
+    sha256 = hashlib.sha256()
+    sha256.update(password_bytes)
+    hash = sha256.hexdigest()
+    print(f"hash of {password} is {hash}")
+    return hash
 
 @route('/')
 def get_index():
@@ -31,7 +40,7 @@ def get_signup(user, password, password2):
     current_user = request.get_cookie("user","-")
     if current_user !="-":
         return "Sorry, you have to sign out first."
-    
+
     if len(user) < 3:
         return "Sorry, the user name requires at least 3 characters."
     if not user.isalnum():
@@ -42,19 +51,39 @@ def get_signup(user, password, password2):
     if not password.isalnum():
         return "Sorry, the password must be letters and digits."
 
-    #store the password
+ #store the password
     with open(f'data/{user}.json',"w") as f:
         json.dump({
-        'password':password
+        'password-hash':hash_password(password)
         }, f)
 
     response.set_cookie("user", user, path='/')
     return "ok, it looks like you logged in"
-       
 
-@route('/login/<user>')
-def get_login(user):
-    current_user = request.get_cookie("user","-")
+
+@route('/login/<user>/<password>')
+def get_login(user, password):
+    #set default response to '-' if login fails
+    response.set_cookie("user", '-', path = '/')
+
+    # sanitize user name so we don't inject malicious filenames
+    if not user.isalnum():
+        return "Sorry, the user must be letters and digits."
+
+    #see if user exists
+    filename = f'data/{user}.json'
+    if not os.path.isfile(filename):
+        return "Sorry, no such user."
+
+    #fetch password
+    with open(f'data/{user}.json',"r") as f:
+        data = json.load(f)
+
+    #check password correctness
+    if data["password-hash"] != hash_password(password):
+        return "Sorry, the user name and password do not match."
+
+    #sucessful login
     response.set_cookie("user", user, path = '/')
     return("ok, it looks like you logged in")
 
